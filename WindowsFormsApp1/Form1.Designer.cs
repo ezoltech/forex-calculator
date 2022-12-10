d094968c9fc494d4aac8a6b333058c4effacab5b
ezoltech <ezramamo13@gmail.com>
2022-12-10 15:39:50 -0800

    first-commit

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_pips = new System.Windows.Forms.TextBox();
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.lbl_result = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txt_lot = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt_amnt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 81);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pips";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 259);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = " Profit/Loss = ";
+            // 
+            // txt_pips
+            // 
+            this.txt_pips.Location = new System.Drawing.Point(183, 76);
+            this.txt_pips.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txt_pips.Name = "txt_pips";
+            this.txt_pips.Size = new System.Drawing.Size(246, 34);
+            this.txt_pips.TabIndex = 2;
+            this.txt_pips.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Location = new System.Drawing.Point(46, 204);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(147, 31);
+            this.btn_calculate.TabIndex = 3;
+            this.btn_calculate.Text = "Calculate";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            this.btn_calculate.Click += new System.EventHandler(this.btn_calculate_Click);
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Location = new System.Drawing.Point(376, 204);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(120, 31);
+            this.btn_clear.TabIndex = 4;
+            this.btn_clear.Text = "Clear";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // lbl_result
+            // 
+            this.lbl_result.AutoSize = true;
+            this.lbl_result.Location = new System.Drawing.Point(279, 259);
+            this.lbl_result.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lbl_result.Name = "lbl_result";
+            this.lbl_result.Size = new System.Drawing.Size(19, 29);
+            this.lbl_result.TabIndex = 5;
+            this.lbl_result.Text = " ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(362, 259);
+            this.label4.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = " $";
+            // 
+            // txt_lot
+            // 
+            this.txt_lot.Location = new System.Drawing.Point(184, 24);
+            this.txt_lot.Margin = new System.Windows.Forms.Padding(5);
+            this.txt_lot.Name = "txt_lot";
+            this.txt_lot.Size = new System.Drawing.Size(246, 34);
+            this.txt_lot.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(82, 29);
+            this.label3.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 29);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Lots";
+            // 
+            // txt_amnt
+            // 
+            this.txt_amnt.Location = new System.Drawing.Point(183, 135);
+            this.txt_amnt.Margin = new System.Windows.Forms.Padding(5);
+            this.txt_amnt.Name = "txt_amnt";
+            this.txt_amnt.Size = new System.Drawing.Size(246, 34);
+            this.txt_amnt.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(81, 140);
+            this.label5.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 29);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Amount";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 297);
+            this.Controls.Add(this.txt_amnt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txt_lot);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lbl_result);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.btn_calculate);
+            this.Controls.Add(this.txt_pips);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Forex Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_pips;
+        private System.Windows.Forms.Button btn_calculate;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Label lbl_result;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txt_lot;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt_amnt;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int money = 10;
+        double lot_size, trading_amount;
+        float pips_size, result;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+            lot_size = double.Parse(txt_lot.Text);
+            pips_size = float.Parse(txt_pips.Text);
+            trading_amount = double.Parse(txt_amnt.Text);
+            lbl_result.Text = (trading_amount * lot_size * pips_size * money).ToString();
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            txt_pips.Text = "";
+            txt_lot.Text = "";
+            txt_amnt.Text = "";
+            lbl_result.Text = "";
+        }
+    }
+}
